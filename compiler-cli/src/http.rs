471a5ca4c37f8712a8ee41f9f059b38e52c834aa
winstxnhdw <winston.ho@redbulltechnology.com>
2024-12-02 16:14:29 +0000

    refactor: propagate errors

--- compiler-cli/src/http.rs
+++ compiler-cli/src/http.rs
@@ -4,8 +4,9 @@ use std::sync::OnceLock;
 use async_trait::async_trait;
 use gleam_core::{Error, Result};
 use http::{Request, Response};
+use reqwest::{Certificate, Client};
 
-static REQWEST_CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
+static REQWEST_CLIENT: OnceLock<Client> = OnceLock::new();
 static CERTS_ENV_VAR: &str = "GLEAM_CACERTS_PATH";
 
 #[derive(Debug)]
@@ -44,31 +45,20 @@ impl gleam_core::io::HttpClient for HttpClient {
     }
 }
 
-fn init_client() -> reqwest::Client {
-    if let Some(cert) = get_certificate() {
-        return reqwest::Client::builder()
+fn init_client() -> Client {
+    match get_certificate() {
+        Ok(cert) => Client::builder()
             .add_root_certificate(cert)
             .build()
-            .expect("Unable to initialize a reqwest HTTP client");
-    } else {
-        return reqwest::Client::new();
+            .expect("Unable to build reqwest client with certificate"),
+        _ => Client::new(),
     }
 }
 
-fn get_certificate() -> Option<reqwest::Certificate> {
-    match std::env::var(CERTS_ENV_VAR) {
-        Ok(certs_path) => {
-            let data = std::fs::read(certs_path).expect(&format!(
-                "Unable to read certs file set as `{}`",
-                CERTS_ENV_VAR
-            ));
-            let cert = reqwest::Certificate::from_pem(&data).expect(&format!(
-                "Unable to construct a certificate from certs file set as `{}`",
-                CERTS_ENV_VAR
-            ));
+fn get_certificate() -> Result<Certificate, Box<dyn std::error::Error>> {
+    let certificate_path = std::env::var(CERTS_ENV_VAR)?;
+    let certificate_bytes = std::fs::read(&certificate_path)?;
+    let certificate = Certificate::from_pem(&certificate_bytes)?;
 
-            Some(cert)
-        }
-        _ => None,
-    }
+    Ok(certificate)
 }
